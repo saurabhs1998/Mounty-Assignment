3fa751268368c155fea8accf81111a97d006f954
saurabh singh <47899416+saurabhs1998@users.noreply.github.com>
2020-04-21 13:22:20 +0530

    new commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="footer">
+    <ul>
+      <li>&#10004; Created by Saurabh Singh</li>
+      <li>&#10004; Created with Vue.js</li>
+      <li>
+        <a
+          href="https://github.com/saurabhs1998/Movies-website-Drive-Assignment-.git"
+        >Repo on github</a>
+      </li>
+      <li>
+        &copy; by
+        <a href>20 April 2020</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  margin: 0 auto;
+  padding: 4em 0;
+  text-align: center;
+  font-size: 0.9em;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+  display: block;
+  padding: 1em;
+}
+a {
+  color: $color-text-primary;
+  text-decoration: dotted underline;
+  &:hover {
+    color: $color-text-secondary;
+    background-color: $color-primary;
+    text-decoration: none;
+  }
+}
+@include sm {
+  .footer {
+    max-width: 1000px;
+  }
+  ul {
+    display: flex;
+  }
+  li {
+    display: inline;
+    padding-right: 2em;
+    text-align: left;
+
+    &:last-child {
+      flex-grow: 1;
+      padding-right: 1em;
+      text-align: right;
+    }
+  }
+}
+</style>
